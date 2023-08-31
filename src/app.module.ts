28784ce3219c604d5a2b987b42f38c6a0e94afe0
Daniel Arenas <jdaniel.arenas@gmail.com>
2023-08-31 17:05:00 -0500

    fix: update app.module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { PlanetModule } from 'modules/planets/planet.module';
+import { PlanetModule } from './modules/planets/planet.module';
 
 @Module({
   imports: [HttpModule, PlanetModule],
